f1610bf3b56cfe18c5d5f3212a289729a66d19e2
sajeeva salgadoe <ssalgadoe@gmail.com>
2018-04-11 19:32:54 -0400

    prediction accuracy test with - KNN- graphical comparision-errors-3D

--- iris_classification_test.py
+++ iris_classification_test.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn import svm, neighbors
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 data = load_iris()
@@ -32,17 +33,23 @@ for i in range(len(test_y)):
     if result[i] == test_y[i]:
         correct+=1
 
-accuracy = [ 0 if result[i]==test_y[i] else 1 for i in range(len(test_y)) ]
+accuracy = [ 5 if result[i]==test_y[i] else 10 for i in range(len(test_y)) ]
 
 print('accuracy', correct/len(test_y))
 
-x_it = np.arange(0,len(test_y),1)
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 
-axes[0,0].scatter(test_x[:,0],test_x[:,1], c=accuracy)
-axes[0,1].scatter(test_x[:,0],test_x[:,2], c=accuracy)
-axes[1,0].scatter(test_x[:,1],test_x[:,2], c=accuracy)
-axes[1,1].scatter(test_x[:,1],test_x[:,3], c=accuracy)
+# axes[0,0].scatter(test_x[:,0],test_x[:,1], c=accuracy)
+# axes[0,1].scatter(test_x[:,0],test_x[:,2], c=accuracy)
+# axes[1,0].scatter(test_x[:,1],test_x[:,2], c=accuracy)
+# axes[1,1].scatter(test_x[:,1],test_x[:,3], c=accuracy)
+
+
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+ax.scatter(test_x[:,0],test_x[:,1], test_x[:,2], c=accuracy, s=50)
+ax.scatter(test_x[:,0],test_x[:,1], test_x[:,2], c=result, s=10)
+
 plt.show()
 
